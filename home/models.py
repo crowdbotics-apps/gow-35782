ecd048f57434c4cb2207f86294e9e938fb708063
Crowdbotics <team@crowdbotics.com>
2022-06-18 01:32:46 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Buyer(models.Model):
+    "Generated Model"
+    buyer = models.BigIntegerField()
+    address = models.BigIntegerField()
